43b455463fa865501c95633b31d864e89473b007
poberWong <poberwong@gmail.com>
2019-05-22 19:44:53 +0800

    add WebView Component which has loading status

---
+++ src/components/WebView.js
@@ -0,0 +1,51 @@
+/**
+ * Created by poberwong on 2019-05-22.
+ */
+
+import React, { Component } from 'react'
+import { View, ActivityIndicator, ViewPropTypes } from 'react-native'
+import WebView from 'react-native-webview'
+import PropTypes from 'prop-types'
+
+export default class extends Component {
+  static propTypes = {
+    source: PropTypes.object,
+    style: ViewPropTypes.style
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: !!props.source
+    }
+  }
+
+  _onLoadEnd = () => {
+    this.setState({loading: false})
+  }
+
+  render () {
+    const {loading} = this.state
+    const {style, ...rest} = this.props
+
+    return (
+      <View style={[{flex: 1}, style]}>
+        <WebView
+          style={{flex: 1}}
+          javaScriptEnabled
+          allowUniversalAccessFromFileURLs
+          allowsInlineMediaPlayback
+          onLoadEnd={this._onLoadEnd}
+          originWhitelist={['file://', 'http://', 'https://']}
+          {...rest}
+        />
+        {
+          loading &&
+          <ActivityIndicator
+            size='large'
+            style={{position: 'absolute', alignSelf: 'center', top: '50%'}} />
+        }
+      </View>
+    )
+  }
+}
